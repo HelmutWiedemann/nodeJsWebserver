27b341167cc4d1802179120e77036633b38ba924
Benedikt Arnold <benedikt.arnold@gmx.net>
2012-08-30 09:56:47 +0200

    added cluster.js and refactored index.js

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-var server = require("./modules/server");
-var router = require("./modules/router");
-var requestHandlerHtml = require("./modules/requestHandlerHtml");
-var requestHandlerExec = require("./modules/requestHandlerExec");
-var requestHandlerIcon = require("./modules/requestHandlerIcon");
-var requestHandlerMysql = require("./modules/requestHandlerMysql");
-
-var handle = {}
-handle["/"] = requestHandlerHtml.showIndex;
-handle["/index.html"] = requestHandlerHtml.showIndex;
-handle["/dir"] = requestHandlerExec.dir;
-handle["/ls"] = requestHandlerExec.ls;
-handle["/favicon.ico"] = requestHandlerIcon.favicon;
-handle["/people"] = requestHandlerMysql.listPeople;
-
-server.start(router.route, handle); 
+var child = require("./modules/child");
+child.start()
